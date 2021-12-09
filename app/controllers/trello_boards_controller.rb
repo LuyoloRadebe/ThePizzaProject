4f29ed5316733c36477a3a74ba29903d9fab33a7
LuyoloRadebe <luyolonradebe@gmail.com>
2021-12-09 09:16:18 +0200

    todo: form input for resource

--- app/controllers/trello_boards_controller.rb
+++ app/controllers/trello_boards_controller.rb
@@ -1,24 +1,22 @@
 class TrelloBoardsController < ApplicationController
   require 'rest-client'
+  #before_action :new_card_params, only: [:new, :create]
 
   def index
   end
   
   def new
-    @submit = new_card_params()
-    puts @submit
+    @list_id = params.fetch(:new,{})
+    puts new_card_params
     render
   end
 
   def create
-    @new_card = new_card_params()
-    puts @submit
-    puts @new_card
-    #@card_name = "#{@request.my_unique_id} - #{@request.user.first_name} #{@request.user.last_name} - entrada #{Date.today.to_s(:long)}"
+    @card_name = params.merge(params.fetch(:,{}))
     #RestClient.post "https://api.trello.com/1/cards?name=#{card_name}&idList=60f98388864f8e839ed71b99&key=#{ENV['PUBLIC_TRELLO_KEY']}&token=#{ENV['MEMBER_TRELLO_TOKEN']}", { accept: :json }
   end
 
   def new_card_params
-    params.fetch(:list_id,{})
+    #params.fetch(:list_id,{})
   end
 end
