389ebc1c377c6ef34b496ab3d0cc86640de9183f
LuyoloRadebe <luyolonradebe@gmail.com>
2021-12-09 17:20:46 +0200

    task_b complete

--- app/controllers/trello_boards_controller.rb
+++ app/controllers/trello_boards_controller.rb
@@ -1,22 +1,16 @@
 class TrelloBoardsController < ApplicationController
   require 'rest-client'
-  #before_action :new_card_params, only: [:new, :create]
-
   def index
   end
   
   def new
-    @list_id = params.fetch(:new,{})
-    puts new_card_params
     render
   end
 
   def create
-    @card_name = params.merge(params.fetch(:,{}))
-    #RestClient.post "https://api.trello.com/1/cards?name=#{card_name}&idList=60f98388864f8e839ed71b99&key=#{ENV['PUBLIC_TRELLO_KEY']}&token=#{ENV['MEMBER_TRELLO_TOKEN']}", { accept: :json }
-  end
-
-  def new_card_params
-    #params.fetch(:list_id,{})
+    list_id = params[:new_card][:list_id]
+    card_name = params[:new_card][:card_name]    
+    RestClient.post "https://api.trello.com/1/cards?name=#{card_name}&idList=#{list_id}&key=#{ENV['TRELLO_API_KEY']}&token=#{ENV['TRELLO_API_TOKEN']}", { accept: :json }
+    redirect_to root_path
   end
 end
